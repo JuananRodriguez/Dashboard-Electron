7650d9de175caa23dfe9e04c885dd801e51eecc8
juanan <juananrodriguezmartinez@gmail.com>
2018-11-09 20:16:00 +0100

    added grid-draggable-layout library

---
+++ app/components/GridLayout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import GridLayout from 'react-grid-layout';
+
+export default class MyFirstGrid extends React.Component {
+  render() {
+    // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+      { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+    ];
+    return (
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div key="a">Grid 1</div>
+        <div key="b">Grid 2</div>
+        <div key="c">Grid 3</div>
+      </GridLayout>
+    );
+  }
+}
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import GridLayout from './GridLayout';
 import routes from '../constants/routes';
 import styles from './Home.css';
 
@@ -14,6 +15,7 @@ export default class Home extends Component<Props> {
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
+        <GridLayout />
       </div>
     );
   }
